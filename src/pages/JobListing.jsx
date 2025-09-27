1717dd69941006fedfaeac5b982368a04036c7b0
ananyyavijay <ananyavijay05@gmail.com>
2025-09-28 01:06:42 +0530

    final deployment

--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -4,7 +4,7 @@ import { State } from "country-state-city";
 import { BarLoader } from "react-spinners";
 import useFetch from "@/hooks/use-fetch";
 
-import JobCard from "@/components/jobCard";
+import JobCard from "@/components/JobCard";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
