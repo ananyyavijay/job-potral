f35a338cfc3e87b6f5db4f2217bcc76f82426864
ananyyavijay <ananyavijay05@gmail.com>
2025-09-28 19:40:39 +0530

    Fix clerk sign-in

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,26 +1,5 @@
-// import { Navigate, useLocation } from "react-router-dom";
-// import { useUser } from "@clerk/clerk-react";
 
-// const ProtectedRoute = ({ children }) => {
-//   const { isSignedIn, isLoaded, user } = useUser();
-//   const { pathname } = useLocation();
-
-//   if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
-//     return <Navigate to="/?sign-in=true" />;
-//   }
-
-//   if (
-//     user !== undefined &&
-//     !user?.unsafeMetadata?.role &&
-//     pathname !== "/onboarding"
-//   )
-//     return <Navigate to="/onboarding" />;
-
-//   return children;
-// };
-
-// export default ProtectedRoute;
-
+/* eslint-disable react/prop-types */
 import { Navigate, useLocation } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
 
@@ -28,17 +7,16 @@ const ProtectedRoute = ({ children }) => {
   const { isSignedIn, isLoaded, user } = useUser();
   const { pathname } = useLocation();
 
-  if (!isLoaded) {
-    return null; // Wait until Clerk loads
+  if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
+    return <Navigate to="/?sign-in=true" />;
   }
 
-  if (!isSignedIn) {
-    return <Navigate to="/sign-in" />; // Use Clerk's sign-in route
-  }
-
-  if (!user?.unsafeMetadata?.role && pathname !== "/onboarding") {
+  if (
+    user !== undefined &&
+    !user?.unsafeMetadata?.role &&
+    pathname !== "/onboarding"
+  )
     return <Navigate to="/onboarding" />;
-  }
 
   return children;
 };
